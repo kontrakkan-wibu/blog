d53737e2ee08ecea265740a074678686f0382644
rizalhamdana <ari.gusti12@gmail.com>
2020-12-31 16:49:46 +0700

    initial commit

---
+++ src/test/java/com/kontrakkan/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kontrakkan.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
